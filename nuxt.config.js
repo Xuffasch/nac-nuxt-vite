85868c0872309b93ebf2264e83941a4051f4b3b5
Xuffasch <tristan.lucas@gmail.com>
2021-03-22 14:36:24 +0100

    Add the vue plugin vue-fragment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 export default {
+  components: true,
   buildModules: ['nuxt-vite', '@nuxtjs/tailwindcss'],
   tailwindcss: { 
     jit: true
-  }
+  },
+  plugins: ['~/plugins/vue-fragment.js']
 }
--- pages/fun.vue
+++ pages/fun.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
+  <fragment >
     <h1>Fun page</h1>
     <p>All the fun when creating pages with Nuxt</p>
     <NuxtLink to='/'>Home page</NuxtLink>
-  </div>
+  </fragment >
 </template>
