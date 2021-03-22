5c52e301a01a962252ba23d424f525420bb6ebf4
Xuffasch <tristan.lucas@gmail.com>
2021-03-22 16:42:38 +0100

    Set header for all pages by the layout component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,23 @@
 <template>
-  <Nuxt />
+  <div v-frag>
+    <header>
+        <h1 v-html="this.title"></h1>
+    </header>
+    <Nuxt />
+  </div>
 </template>
+
+<script>
+  import frag from 'vue-frag'
+  export default ({
+    directives: {
+      frag
+    },
+    created() {
+      this.title = 'Album Comics Clone with Nuxt2 and Vite'
+    },
+    data: {
+      title: ''
+    }
+  })
+</script>
