9ced41263611c7eff25f85b29cd15573cc650590
Xuffasch <tristan.lucas@gmail.com>
2021-03-22 10:58:13 +0100

    Initialize pages

---
+++ pages/fun.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Fun page</h1>
+    <p>All the fun when creating pages with Nuxt</p>
+    <NuxtLink to='/'>Home page</NuxtLink>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1>Hello World!</h1>
+    <NuxtLink to='/fun'>Fun page</NuxtLink>
+  </main>
+</template>
+
+<style scoped>
+  h1 {
+    @apply p-2 m-2 text-lg text-albumBlue;
+  }
+  a {
+    @apply m-2 p-2 block bg-yellow-400 text-lg;
+  }
+
+</style>
